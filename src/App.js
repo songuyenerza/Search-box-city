bbd44fa5905001997dbd3e5821eb5b2eecc043cb
songuyen <PhuongDaoHust>
2022-08-23 21:54:03 +0700

    bopw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { Select, Button } from 'antd';
+import { useState } from 'react';
+import cities from 'cities.json';
+
+const { Option } = Select;
+let timeout;
+let currentValue;
+
+const fetch = (value, callback) => {
+  if (timeout) {
+    clearTimeout(timeout);
+    timeout = null;
+  }
+
+  currentValue = value;
+
+  const fake = () => {
+    const a = [];
+    console.log(cities[0].name);
+    if (value) {
+      cities.forEach((e, i) => {
+        if (e.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())) a.push({
+          value: i,
+          text: e.name
+        })
+      })
+      callback(a.reverse())
+    }
+    else {
+      callback(cities.map((e, i) => ({
+        value: i,
+        text: e.name
+      })))
+    }
+
+  };
+
+  timeout = setTimeout(fake, 300);
+};
 
 function App() {
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState();
+
+  const handleSearch = (newValue) => {
+    if (newValue) {
+      fetch(newValue, setData);
+    } else {
+      setData([]);
+    }
+  };
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+  console.log(data);
+  const options = data?.map((d) => <Option key={d.value}>{d.text}</Option>);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select
+        placeholder='nhap vao day...'
+        showSearch
+        value={value}
+        defaultActiveFirstOption={false}
+        showArrow={false}
+        filterOption={false}
+        onSearch={handleSearch}
+        onChange={handleChange}
+        notFoundContent={null}
+        style={{ width: '300px' }}
+      >
+        {options}
+      </Select>
+      <Button>search...</Button>
     </div>
   );
 }
